3aa3181f019a7a49ace6835aa372c784d1dbe26b
unknown <morgan.mbarki@gmail.com>
2019-12-09 10:42:07 +0100

    end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-var name = ['Morgan', 'Louis', 'Raphael', 'Azzedine', 'Lucas', 'Marie-Helene', 'Jordan', 'Pauline', 'Emilie', 'Severine', 'Marie', 'Melanie', 'Audrey'];
+var pers = ['Morgan', 'Louis', 'Raphael', 'Azzedine', 'Lucas', 'Marie-Helene', 'Jordan', 'Pauline', 'Emilie', 'Severine', 'Marie', 'Melanie', 'Audrey'];
 var button = document.getElementById('randomiz');
 button.addEventListener('click',ask);
 function ask() {
-  var randomNumber = Math.floor(Math.random()*12);
-  var result = name[randomNumber];
-  console.log(name.length);
+  var randomNumber = Math.floor(Math.random() * 12);
+  var result = pers[randomNumber];
+  alert(result);
   // var item = name[Math.floor(Math.random()*name.length)];
   // alert(item)
 }
